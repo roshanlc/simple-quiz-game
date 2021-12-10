38c9cae9a68729da8c82ed1abc48d2f7d9a1d33c
Roshan Lamichhane <lamichhanersn@gmail.com>
2021-12-10 07:16:16 +0545

    Initial version

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Quiz struct {
+	Question, Answer string
+}
+
+func main() {
+
+	args := os.Args
+	fmt.Println(args)
+
+	// Open the mentioned file
+	file, err := os.Open("problems.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Closes the file after the reading it
+	defer file.Close()
+
+	// Read the csv records data
+	records, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	QuizGame(records)
+}
+
+// The quiz game is performed here
+// after taking the csv data records
+func QuizGame(records [][]string) {
+
+	total := len(records)
+	correct := 0
+	solved := 0
+
+	fmt.Println("Welcome To Quiz Game !")
+	fmt.Println("Type 's' to stop the game.\n Any other value will be considered an answer\n")
+
+	for i, values := range records {
+
+		fmt.Printf("Question #%d : %s\n", i+1, values[0])
+		fmt.Printf("Answer : ")
+		ans := ReadInput()
+
+		if ans == "s" || ans == "S" {
+			fmt.Println("Game stopped!")
+			break
+		}
+
+		if ans == values[1] {
+			correct++
+		}
+		solved++
+		fmt.Println()
+	}
+
+	if solved == total {
+		fmt.Println("Bravo!! You attempted all the questions.\n")
+	}
+
+	fmt.Println("---------------------------------")
+	fmt.Println("Total questions solved : ", solved)
+	fmt.Println("Total correct answers  : ", correct)
+
+	fmt.Println("---------------------------------")
+}
+
+// Reads the input from command line and returns as string
+func ReadInput() string {
+
+	var input string
+
+	fmt.Scanf("%s", &input)
+
+	return input
+
+}
